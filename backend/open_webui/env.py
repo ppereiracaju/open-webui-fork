4ef192c0b42d0d6042a5387eb08beb524f59cfa4
ppereiracaju <93482183+ppereiracaju@users.noreply.github.com>
2025-01-01 12:11:40 -0300

    na tela de login retirar OpenAI WebUI

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -101,7 +101,7 @@ log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
 if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+    WEBUI_NAME += ""
 
 WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
 
